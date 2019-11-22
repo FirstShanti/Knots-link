c4802051e79d45569c9c44fb43ff999543fc0a83
FirstShanti <steppe.alone@gmail.com>
2019-11-22 21:45:23 +0200

    add blueprints models

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask 
 from config import Configuration
+
+from flask_sqlalchemy import SQLAlchemy
+
 #, render_template, redirect, url_for, request
 #from flask_sqlalchemy import SQLAlchemy
 #import psycopg2
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+db = SQLAlchemy(app)
+
+
 
 '''POSTGRES = {
     'user': 'postgres',
