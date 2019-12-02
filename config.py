b5981437044c08a2cef2a9e351b48d86e19d75f3
FirstShanti <Steppe.alone@gmail.com>
2019-12-03 00:14:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:44Vl@dislav89Shev4enko05@localhost:3306/test2'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:*****localhost:3306/test2'
