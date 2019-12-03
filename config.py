88222fc1550e0349c7870e2e822126e63cb847ef
FirstShanti <steppe.alone@gmail.com>
2019-12-04 01:28:32 +0200

    add environ DB_NAME

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@localhost:3306/test2'\
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@localhost:3306/{}'\
                                .format(os.environ['DB_USERNAME'],
-                                       os.environ['DB_PASSWORD'])
+                                       os.environ['DB_PASSWORD'],
+                                       os.environ['DB_NAME'])
