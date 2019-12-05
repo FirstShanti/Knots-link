83974a4a25636a5c5e20e49921c6fa9142a62874
FirstShanti <steppe.alone@gmail.com>
2019-12-05 23:05:26 +0200

    add form to create post with tag

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,11 +1,45 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from models import Post, Tag
 import locale
+from .forms import PostForm
+from app import db
+import re
 
 locale.setlocale(locale.LC_ALL, "")
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 about = Blueprint('about', __name__, template_folder='templates')
+
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+
+	form = PostForm()
+
+	if request.method == 'POST':
+		title = request.form['title']
+		body = request.form['body']
+		t = re.split(r'[\s, .]', request.form['name'])
+
+		try:
+			if title and body and t:
+				post = Post(title=title, body=body)
+				for i in t:
+					if Tag.query.filter_by(name=i).first():
+						tag = Tag.query.filter_by(name=i).first()
+					else:
+						tag = Tag(name=i)
+					post.tags.append(tag)	
+				db.session.add(post)
+				db.session.commit()
+		except:
+			print('Something wrong')
+
+		return redirect( url_for('posts.index') )
+
+	
+	return render_template('posts/create_post.html', form=form)
+
 
 @posts.route('/')
 def index():
@@ -15,11 +49,11 @@ def index():
 	if q:
 		posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
 	else:
-		posts = Post.query.all()
+		posts = Post.query.all()[-1::-1]
 	
 	return render_template('posts/index.html', posts=posts)
 
-# http://localhost/blog/first_post
+
 @posts.route('/<slug>')
 def post_content(slug):
     post = Post.query.filter(Post.slug==slug).first()
