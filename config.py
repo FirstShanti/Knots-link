8e74f2a151bebad99c5ffb3e1d795e29cb6beaac
FirstShanti <steppe.alone@gmail.com>
2019-12-05 22:58:52 +0200

    add secret key

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@localhost:3306/{}'\
                                .format(os.environ['DB_USERNAME'],
                                        os.environ['DB_PASSWORD'],
-                                       os.environ['DB_NAME'])
+                                       os.environ['DB_NAME']
+                                       )
