59f25cbb7f2b99464dfdfcd940fd800541bc3293
FirstShanti <steppe.alone@gmail.com>
2020-01-04 00:17:52 +0200

    remove forms.py

--- posts/forms.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form, StringField, TextAreaField, validators
-
-
-
-class PostForm(Form):
-	title = StringField('Title', [validators.Length(min=4, max=78)])
-	body = TextAreaField('Body', [validators.Length(min=250, max=50000)])
-	name = StringField('Tag', [validators.Length(min=3, max=100)])
-
