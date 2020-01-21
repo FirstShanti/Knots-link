e892df611b5948c85e9b4f35934a737ef04d423a
FirstShanti <steppe.alone@gmail.com>
2020-01-21 22:14:39 +0200

    divide and get db-uri in two class sqlite and MySQL

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ import os
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'one-and-only-one-anna'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://{}:{}@localhost:3306/{}'.format(
-    	os.environ['DB_USERNAME'],
-    	os.environ['DB_PASSWORD'],
-    	os.environ['DB_NAME']
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     CKEDITOR_PKG_TYPE = "standard"
 
+
+class Home(Configuration):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DB').format(
+	    	os.environ['DB_USERNAME'],
+	    	os.environ['DB_PASSWORD'],
+	    	os.environ['DB_NAME']
+	    )
+
+class Work(Configuration):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
