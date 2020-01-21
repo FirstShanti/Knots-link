78ffc19d8bbad174cf5661bcbc3e051c0cb31317
FirstShanti <steppe.alone@gmail.com>
2020-01-21 22:10:17 +0200

    get db-uri from environ

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+
+import os
 from flask import Flask
-from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
-
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-
+from config import * 
 
 app = Flask(__name__)
-app.config.from_object(Configuration)
+app.config.from_object(eval(os.environ.get('ENV')))
 
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
