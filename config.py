39985643838c0bdb15e42cefe51aa2757698acfd
FirstShanti <steppe.alone@gmail.com>
2020-01-22 20:44:03 +0200

    update config for two env with different db

--- config.py
+++ config.py
@@ -1,18 +1,24 @@
 import os
 
 class Configuration(object):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    CKEDITOR_PKG_TYPE = "standard"
+	DEBUG = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	CKEDITOR_PKG_TYPE = "standard"
 
 
 class Home(Configuration):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DB').format(
-	    	os.environ['DB_USERNAME'],
-	    	os.environ['DB_PASSWORD'],
-	    	os.environ['DB_NAME']
-	    )
+	try:
+		SQLALCHEMY_DATABASE_URI = os.environ.get('DB').format(
+			os.environ['DB_USERNAME'],
+			os.environ['DB_PASSWORD'],
+			os.environ['DB_NAME']
+		)
+	except Exception as e:
+		print(e.__class__)
 
 class Work(Configuration):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
+	try:
+		SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
+	except Exception as e:
+		print(e.__class__)
