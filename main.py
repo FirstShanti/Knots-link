1a14e80123497e583a5dbec49673c31c71baeb3a
FirstShanti <steppe.alone@gmail.com>
2020-01-27 06:46:32 +0200

    rename blueprint about to user_profile

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from app import app
 from app import db
 import view
 
-from posts.blueprint import posts, about
+from posts.blueprint import posts, user_profile
 from login.login import login
 
 app.register_blueprint(login, url_prefix='/')
 app.register_blueprint(posts, url_prefix='/blog')
-app.register_blueprint(about, url_prefix='/contacts')
+app.register_blueprint(user_profile, url_prefix='/contacts')
 
 
 if __name__ == '__main__':
