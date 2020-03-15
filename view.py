f62ffc703ca695f3d16ab204e59bc3b741934d20
FirstShanti <steppe.alone@gmail.com>
2020-03-15 02:36:51 +0200

    add 404 page

--- view.py
+++ view.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def error_404(page):
+	return render_template('error404.html')
