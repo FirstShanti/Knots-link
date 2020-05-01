6046828fc635efbf9f54e5dc180b52c31555ebf7
FirstShanti <you@example.com>
2020-05-01 23:00:08 +0300

    add endpointname for jinja

--- view.py
+++ view.py
@@ -1,10 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+	return render_template('index.html', endpoint=request.endpoint)
 
 @app.errorhandler(404)
 def error_404(page):
