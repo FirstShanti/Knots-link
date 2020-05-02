d8ad9ea6ef2351d20617b9bb11af27cf20f620eb
FirstShanti <you@example.com>
2020-05-02 17:57:21 +0300

    add admin route

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from app import app
-from app import db
 import view
-
 from posts.blueprint import posts, user_profile
 from login.login import login
+from admin.admin import admin 
 
 app.register_blueprint(login, url_prefix='/')
 app.register_blueprint(posts, url_prefix='/blog')
 app.register_blueprint(user_profile, url_prefix='/contacts')
+app.register_blueprint(admin, url_prefix='/admin')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5555)
