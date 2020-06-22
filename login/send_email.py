f5189638f4cd0acd845ac10acd64edb9c337fdc2
FirstShanti <steppe.alone@gmail.com>
2020-06-23 00:40:19 +0300

    add send post to email

--- login/send_email.py
+++ login/send_email.py
@@ -2,7 +2,7 @@ import smtplib, os
 from email.message import EmailMessage
 from flask import render_template, request, flash
 
-def send_email(user):
+def send_email(user, msg=None):
 	
 	login = os.environ.get('MAIL_USERNAME')
 	password = os.environ.get('MAIL_PASSWORD')
@@ -10,12 +10,16 @@ def send_email(user):
 	toaddr = user.email
 
 	email = EmailMessage()
-	email['Subject'] = "authentication"
 	email['From'] = fromaddr
 	email['To'] = toaddr
 
-	url = f'{request.url_root}auth/?user={user.slug}&key={user.auth_key}'
-	email.set_content(render_template('confirmed.html', url=url), subtype='html')
+	if not msg:
+		email['Subject'] = "authentication"
+		url = f'{request.url_root}auth/?user={user.slug}&key={user.auth_key}'
+		email.set_content(render_template('confirmed.html', url=url), subtype='html')
+	else:
+		email['Subject'] = "Good job!"
+		email.set_content(render_template('post_to_email.html', post=msg), subtype='html')
 	# Send the message via local SMTP server.
 	with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
 		server.ehlo()
